bc2e7dd3f352f6b3c4516b4c758bf6b538f65d17
kostia18 <amokksol@gmail.com>
2020-12-15 16:24:09 +0300

    Добавление 4 кнопки, использование Image Button, Создание Лога для основного жизненного цикла приложения. Кнопка назад - работает, вопросы меняются при нажатии на текстовое поле вопроса

--- app/src/main/java/com/amokksol/geoquiz/MainActivity.kt
+++ app/src/main/java/com/amokksol/geoquiz/MainActivity.kt
@@ -2,18 +2,23 @@ package com.amokksol.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.ImageButton
+//создание консатнты для записей LOG
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     //объявляем наши кнопки с импортом виджета
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var backButton: ImageButton
     private lateinit var questionTextView: TextView
 
 
@@ -28,37 +33,59 @@ class MainActivity : AppCompatActivity() {
     private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //регистрация Логов
+        Log.d(TAG,"onCreate(Bundle&) called")
         setContentView(R.layout.activity_main)
         //создаем ссылку на кнопки
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
         questionTextView = findViewById(R.id.question_text_view)
+
         //создаем действие на нажатие
         trueButton.setOnClickListener { view: View ->
-            var toast1 = Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                toast1.show()
-                toast1.setGravity(Gravity.TOP,0,0)
+            checkAnswer(true)
            //код выполнение - после нажатия -> уведомление
             }
         falseButton.setOnClickListener { view: View ->
             //код выполнение - после нажатия -> уведомление
-            var toast = Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                toast.show()
-                toast.setGravity(Gravity.TOP,0,0)
+         checkAnswer(false)
             }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex+1)%questionBank.size
             updateQuestion()
             }
+        questionTextView.setOnClickListener { currentIndex = (currentIndex+1)%questionBank.size
+            updateQuestion() }
+        backButton.setOnClickListener {
+            currentIndex = (currentIndex-1)%questionBank.size
+            updateQuestion()
+        }
         updateQuestion()
         }
+
+    //Переопрелделние функций жизненного цикла
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG,"onStart(Bundle&) called")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG,"onResume(Bundle&) called")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG,"onPause(Bundle&) called")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG,"onStop(Bundle&) called")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG,"onDestroy(Bundle&) called")
+    }
     //создаем функцию которая нужна  в двух местах
     //вывод вопроса из списка. questionBank - создали список, currentIndex  = 0
     //textResId - значение полученное из Data класса
@@ -66,4 +93,17 @@ class MainActivity : AppCompatActivity() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if(userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show()
+    }
+
+
 }
+
